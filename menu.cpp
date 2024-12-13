c952037e366c7bbbc36bf1989195641c2edb2921
wahyu-nurhadi0915 <muhammadwahyunurhadi@gmail.com>
2024-12-13 14:00:16 +0800

    u:penyesuaian isi pada halaman depan

---
+++ menu.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+void dMenu(){
+system("cls");
+cout<<"Aplikasi Sorting Bubble"<<"\n";       
+cout<<"1. Masukkan data"<<"\n";            
+cout<<"2. Tampilkan data"<<"\n";            
+cout<<"3. Sorting asc"<<"\n";           
+cout<<"4. Sorting dsc"<<"\n";            
+cout<<"5. Exit"<<"\n";           
+cout<<"Masukan angka :";        
+
+}
+
+void mPertama(string pesan){
+system("cls");
+cout<<"hallo saya menu "<<pesan;
+getch();
+}
+
+
+int main() {
+char pl;
+do
+{
+    dMenu();
+    pl=getch();
+  switch (pl)
+  {
+   case '1':
+    /* code */
+    mPertama("pertama");
+    break;
+   case '2':
+    mPertama("ke- dua");
+    /* code */ 
+    break;  
+   case '3':
+    mPertama("ke- tiga");
+    /* code */
+    break;  
+   case '4':
+    mPertama("ke- empat");
+    /* code */
+    break;  
+  case '5':
+    /* code */
+    break;
+  
+  default:
+    system("cls");
+    cout<<"Pilihan Tidak Tersedia";
+    getch();
+    break;
+  }
+
+
+} while (pl!='5');
+  return 0;
+}
